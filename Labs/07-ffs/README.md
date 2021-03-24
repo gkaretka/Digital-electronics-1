ba10483a1b6842a95fa0fccd329d9096a2945343
Gregor Karetka <karetka.gregor@gmail.com>
2021-03-24 17:11:27 +0100

    Add 07

---
+++ Labs/07-ffs/README.md
@@ -0,0 +1,34 @@
+# Digital-electronics-1
+
+## My GitHub repository
+
+[Digital-electronics-1](https://github.com/gkaretka/Digital-electronics-1)
+
+### Preparation tasks:
+
+Write characteristic equations and complete truth tables for D, JK, T flip-flops.
+
+   | **D** | **Qn** | **Q(n+1)** | **Comments** |
+   | :-: | :-: | :-: | :-- |
+   | 0 | 0 | 0 | No change |
+   | 0 | 1 | 0 | Change |
+   | 1 | 1 | 1 | No change |
+   | 1 | 0 | 1 | Change |
+
+   | **J** | **K** | **Qn** | **Q(n+1)** | **Comments** |
+   | :-: | :-: | :-: | :-: | :-- |
+   | 0 | 0 | 0 | 0 | No change |
+   | 0 | 0 | 1 | 1 | No change |
+   | 0 | 1 | 0 | 0 | Reset |
+   | 0 | 1 | 1 | 0 | Reset |
+   | 1 | 0 | 0 | 1 | Set |
+   | 1 | 0 | 1 | 1 | Set |
+   | 1 | 1 | 0 | 1 | Toggle |
+   | 1 | 1 | 1 | 0 | Toggle |
+
+   | **T** | **Qn** | **Q(n+1)** | **Comments** |
+   | :-: | :-: | :-: | :-- |
+   | 0 | 0 | 0 | No change |
+   | 0 | 1 | 1 | No change |
+   | 1 | 0 | 1 | Toggle |
+   | 1 | 1 | 0 | Toggle |
---
+++ Labs/07-ffs/flip_flops_and_latches/flip_flops_and_latches.srcs/sim_1/new/tb_jk_ff_rst.vhd
@@ -0,0 +1,138 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/24/2021 11:27:52 AM
+-- Design Name: 
+-- Module Name: tb_jk_ff_rst - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_jk_ff_rst is
+end tb_jk_ff_rst;
+
+architecture Behavioral of tb_jk_ff_rst is
+    constant c_CLK_100MHZ_PERIOD : time    := 10 ns;
+   
+    signal s_clk : STD_LOGIC;
+    signal s_j : STD_LOGIC;
+    signal s_k : STD_LOGIC;
+    signal s_rst : STD_LOGIC;
+    signal s_q : STD_LOGIC;
+    signal s_q_bar : STD_LOGIC;
+begin
+
+uut_d_ff_arst : entity work.jk_ff_rst
+    port map (
+        clk     =>  s_clk,
+        j       =>  s_j,
+        k       =>  s_k,
+        rst     =>  s_rst,
+        q       =>  s_q,
+        q_bar   =>  s_q_bar
+    );
+    
+    --------------------------------------------------------------------
+    -- Clock generation process
+    --------------------------------------------------------------------
+    p_clk_gen : process
+    begin
+        while now < 750 ns loop         -- 75 periods of 100MHz clock
+            s_clk   <= '0';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+            s_clk   <= '1';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+        end loop;
+        wait;
+    end process p_clk_gen;
+    
+    --------------------------------------------------------------------
+    -- Reset generation process
+    --------------------------------------------------------------------
+    p_reset_gen : process
+    begin
+        s_rst   <= '0';
+        wait for 100 ns;
+        s_rst   <= '1';                 -- Reset activated
+        wait for 80 ns;
+        s_rst   <= '0';
+        wait;
+    end process p_reset_gen;
+
+    p_stimulus : process
+    begin
+        -- Report a note at the begining of stimulus process
+        report "Stimulus process started. ---------------------------------------" severity note;
+        
+        -- Set 1
+        s_j <=  '1';
+        s_k <=  '0'; 
+        wait for 10ns;
+        
+        -- Reset 1
+        s_j <=  '0';
+        s_k <=  '1';
+        wait for 10ns;
+        
+        -- Set 2
+        s_j <=  '1';
+        s_k <=  '0';
+        wait for 10ns;
+        
+        -- Reset 2
+        s_j <=  '0';
+        s_k <=  '1';    
+        wait for 10ns;
+        
+        -- Toggle 1
+        s_j <=  '1';
+        s_k <=  '1';
+        wait for 10ns;
+        
+        -- Toggle 2
+        s_j <=  '1';
+        s_k <=  '1';
+        wait for 10ns;
+        
+        -- Toggle 3
+        s_j <=  '1';
+        s_k <=  '1';
+        wait for 10ns;        
+        
+        -- No change 1
+        s_j <=  '0';
+        s_k <=  '0';
+        wait for 10ns;
+        
+        -- No change 1
+        s_j <=  '0';
+        s_k <=  '0';
+        
+        report "Stimulus process ended. ---------------------------------------" severity note;
+        wait;
+    end process p_stimulus;
+
+end Behavioral;
