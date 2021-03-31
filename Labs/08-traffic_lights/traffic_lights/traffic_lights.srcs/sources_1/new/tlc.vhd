91b8fb85235abb295a8cebc91f5c7d6993b4d01a
Gregor Karetka <karetka.gregor@gmail.com>
2021-03-31 12:05:04 +0200

    Add 2

--- Labs/08-traffic_lights/traffic_lights/traffic_lights.srcs/sources_1/new/tlc.vhd
+++ Labs/08-traffic_lights/traffic_lights/traffic_lights.srcs/sources_1/new/tlc.vhd
@@ -49,13 +49,132 @@ architecture Behavioral of tlc is
     
     signal  s_en    :   std_logic;
     
-    signal  s_cnt   :   unsigned(5 - 1 downto 0);
+    signal  s_cnt   :   std_logic_vector(5 - 1 downto 0);
     
-    constant c_DELAY_GO     :   unsigned(5 - 1 downto 0)  := b"1_0000";
-    constant c_DELAY_WAIT   :   unsigned(5 - 1 downto 0)  := b"0_1000";
-    constant c_DELAY_1S     :   unsigned(5 - 1 downto 0)  := b"0_0100";
+    constant c_DELAY_4SEC   :   unsigned(5 - 1 downto 0)  := b"1_0000";
+    constant c_DELAY_2SEC   :   unsigned(5 - 1 downto 0)  := b"0_1000";
+    constant c_DELAY_1SEC   :   unsigned(5 - 1 downto 0)  := b"0_0100";
     constant c_ZERO         :   unsigned(5 - 1 downto 0)  := b"0_0000";
 begin
+    
+    e_en : entity work.clock_enable
+    generic map(
+        g_MAX =>  4000000
+    )
+    Port map (
+        clk         =>  clk,
+        reset       =>  reset,
+        ce_o        =>  s_en
+    );
+    
+    p_traffic_fsm : process(clk)
+    begin
+        if rising_edge(clk) then
+            if (reset = '1') then       -- Synchronous reset
+                s_state <= STOP1 ;      -- Set initial state
+                s_cnt   <= std_logic_vector(c_ZERO);      -- Clear all bits
 
+            elsif (s_en = '1') then
+                -- Every 250 ms, CASE checks the value of the s_state 
+                -- variable and changes to the next state according 
+                -- to the delay value.
+                case s_state is
+
+                    -- If the current state is STOP1, then wait 1 sec
+                    -- and move to the next GO_WAIT state.
+                    when STOP1 =>
+                        -- Count up to c_DELAY_1SEC
+                        if (unsigned(s_cnt) < c_DELAY_1SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            -- Move to the next state
+                            s_state <= WEST_GO;
+                            -- Reset local counter value
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+
+                    when WEST_GO =>
+                        if (unsigned(s_cnt) < c_DELAY_4SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            s_state <= WEST_WAIT;
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+                        
+                    when WEST_WAIT =>
+                        if (unsigned(s_cnt) < c_DELAY_2SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            s_state <= STOP2;
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+                        
+                    when STOP2 =>
+                        if (unsigned(s_cnt) < c_DELAY_1SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            s_state <= SOUTH_GO;
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+                        
+                    when SOUTH_GO =>
+                        if (unsigned(s_cnt) < c_DELAY_4SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            s_state <= SOUTH_WAIT;
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+                        
+                    when SOUTH_WAIT =>
+                        if (unsigned(s_cnt) < c_DELAY_2SEC) then
+                            s_cnt <= std_logic_vector(unsigned(s_cnt) + 1);
+                        else
+                            s_state <= STOP1;
+                            s_cnt   <= std_logic_vector(c_ZERO);
+                        end if;
+                    
+                    -- It is a good programming practice to use the 
+                    -- OTHERS clause, even if all CASE choices have 
+                    -- been made. 
+                    when others =>
+                        s_state <= STOP1;
+
+                end case;
+            end if; -- Synchronous reset
+        end if; -- Rising edge
+    end process p_traffic_fsm;
+
+    p_output_fsm : process(s_state)
+    begin
+        case s_state is
+            when STOP1 =>
+                south_o <= "100";   -- Red (RGB = 100)
+                west_o  <= "100";   -- Red (RGB = 100)
+                
+            when WEST_GO =>
+                south_o <= "100";   -- Red (RGB = 100)
+                west_o  <= "010";   -- Red (RGB = 010)
+                
+            when WEST_WAIT =>
+                south_o <= "100";   -- Red (RGB = 100)
+                west_o  <= "110";   -- Red (RGB = 011)
+            
+            when STOP2 =>
+                south_o <= "100";   -- Red (RGB = 100)
+                west_o  <= "100";   -- Red (RGB = 100)
+                
+            when SOUTH_GO =>
+                south_o <= "010";   -- Red (RGB = 010)
+                west_o  <= "100";   -- Red (RGB = 100)
+                
+            when SOUTH_WAIT =>
+                south_o <= "110";   -- Red (RGB = 011)
+                west_o  <= "100";   -- Red (RGB = 100)                
+
+            when others =>
+                south_o <= "100";   -- Red
+                west_o  <= "100";   -- Red
+        end case;
+    end process p_output_fsm;
 
 end Behavioral;
